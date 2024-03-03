e3bd2c610998331b985278a41ade9a04224e5785
kamiKAZIK <eduardas.kazakas@gmail.com>
2024-03-03 22:43:58 +0200

    Added log

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
+use log::info;
+
+use crate::configuration::Settings;
+
 mod configuration;
 mod startup;
 mod messages;
 mod models;
 mod storage;
 
-use crate::configuration::Settings;
-
 #[tokio::main]
 async fn main() {
-    let settings: Settings = Settings::new().unwrap();
+    log4rs::init_file("config/log4rs.yaml", Default::default()).unwrap();
+    
+    let settings = Settings::new().unwrap();
+
+    info!("Starting refinery service...");
 
     startup::run(settings).await;
 }
